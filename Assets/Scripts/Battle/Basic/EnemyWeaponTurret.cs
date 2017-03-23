f0708365feef8a25aa484be770e89a869a9d2e47
DragoonKiller <94742643@qq.com>
2017-03-23 21:01:35 +0800

    weapon code rebuild.

---
+++ Assets/Scripts/Battle/Basic/EnemyWeaponTurret.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Enemy weapons use some unique logic instead of player Device.
+/// </summary>
+abstract public class EnemyWeaponTurret : EnemyWeapon
+{
+    /// <summary>
+    /// Default direct cannonball aim to the player.
+    /// </summary>
+    /// <param name="targets"></param>
+    public override void Launch(GameObject[] targets)
+    {
+        // Randomly choose a target in the targets list.
+        int r = Mathf.FloorToInt(Random.Range(0, targets.Length));
+        GameObject t = targets[r];
+
+        // There's nothing can be attacked anymore!
+        // And then it stop attack.
+        if (targets == null || t == null) return;
+        GameObject x = Instantiate(source);
+        x.tag = "EnemyCannonball";
+        x.transform.position = this.gameObject.transform.position;
+        // Source cannonball's direction is (and should be) always up.
+        // Set the cannonball directing to the target.
+        Quaternion q = Quaternion.FromToRotation(Vector2.up,
+            t.transform.position - this.gameObject.transform.position);
+        x.transform.rotation = q;
+    }
+}
